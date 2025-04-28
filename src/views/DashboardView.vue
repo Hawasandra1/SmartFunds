bf87f2e8e4a41c803f3c56427b78a41b46580e14
Hawasandra1 <Hawasandra.m@gmail.com>
2025-04-28 14:22:38 +0300

    Update DashboardView.vue

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -31,7 +31,7 @@
 <template>
   <div class="container mx-auto py-12 px-6 pt-16 md:pt-0">
     <div class="text-center mb-8">
-      <p class="text-xl text-black-700 mb-2">Hi👋</p>
+      <p class="text-xl text-gray-700 mb-2">Hi 👋 Your financial insights will be displayed below</p>
       <h1 class="text-3xl font-bold text-gray-900">Your Financial Dashboard</h1>
     </div>
 
